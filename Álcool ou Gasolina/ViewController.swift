cf451a9ba89e9340928963ebdf1727de584767c0
Ícaro Brandão <icarobrandao@MacBook-Pro-de-Icaro.local>
2017-01-01 00:49:24 -0300

    Initial Commit

---
+++ Álcool ou Gasolina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Álcool ou Gasolina
+//
+//  Created by Ícaro Brandão on 01/01/17.
+//  Copyright © 2017 Ícaro Brandão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
