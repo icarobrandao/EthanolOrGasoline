5ab198e3515b18567f4b1ae5306c82615df40ceb
Ícaro Brandão <icarobrandao@MacBook-Pro-de-Icaro.local>
2017-01-01 23:07:02 -0300

    my first commit

--- Álcool ou Gasolina/ViewController.swift
+++ Álcool ou Gasolina/ViewController.swift
@@ -9,10 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    
+    @IBOutlet weak var ethanolTextField: UITextField!
+    @IBOutlet weak var gasolineTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        var ethanolPrice: Double
+        var eth:Double = 0
+        var gasolinePrice: Double
+        var gas: Double = 0
+        var result: Double
+       
+        if let ethanol = ethanolTextField.text {
+            if ethanol != ""{
+                if let ethanolDouble = Double(ethanol){
+                    eth = ethanolDouble
+                }
+            }
+        }
+        
+        if let gasoline = gasolineTextField.text {
+            if gasoline != ""{
+                if let gasolineDouble = Double(gasoline){
+                    gas = gasolineDouble
+                }
+            }
+        }
+        ethanolPrice = eth
+        gasolinePrice = gas
+        result = ethanolPrice/gasolinePrice
+        
+        if result<0.7 {
+            resultLabel.text = "You should use Ethanol!"
+        }else {
+            resultLabel.text = "You should use Alcohol!"
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
